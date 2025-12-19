37cde8acd8ae8ef3a404d149a0a52a27a7fd6364
Lanh Hoang <lanhhoang@users.noreply.github.com>
2025-12-18 20:52:01 -0500

    Delete Portfolio v2

--- components/loading-spinner.tsx
+++
@@ -1,7 +0,0 @@
-export default function LoadingSpinner() {
-  return (
-    <div className="flex items-center justify-center rounded-full w-14 h-14 bg-gradient-to-tr from-indigo-500 to-pink-500 dark:from-[#946263] dark:to-[#676394]">
-      <div className="h-9 w-9 rounded-full bg-gray-200 dark:bg-gray-600"></div>
-    </div>
-  );
-}
--- sanity/schemas/index.ts
+++
@@ -1,8 +0,0 @@
-import experience from "./experience.schema";
-import page from "./page.schema";
-import profile from "./profile.schema";
-import project from "./project.schema";
-
-const schemas = [experience, profile, project, page];
-
-export default schemas;
