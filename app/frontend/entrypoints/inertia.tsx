6ae656c8fff7f4ebe31c972e2795a66455aea766
Lanh Hoang <lanhhoang@users.noreply.github.com>
2025-12-24 23:36:56 -0500

    Update Biome rules and run biome format

--- app/frontend/entrypoints/inertia.tsx
+++ app/frontend/entrypoints/inertia.tsx
@@ -1,6 +1,6 @@
-import { createInertiaApp, type ResolvedComponent } from '@inertiajs/react'
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import { createInertiaApp, type ResolvedComponent } from "@inertiajs/react"
+import { StrictMode } from "react"
+import { createRoot } from "react-dom/client"
 
 void createInertiaApp({
   // Set default page title
@@ -14,9 +14,12 @@ void createInertiaApp({
   // progress: false,
 
   resolve: (name) => {
-    const pages = import.meta.glob<{default: ResolvedComponent}>('../pages/**/*.tsx', {
-      eager: true,
-    })
+    const pages = import.meta.glob<{ default: ResolvedComponent }>(
+      "../pages/**/*.tsx",
+      {
+        eager: true,
+      },
+    )
     const page = pages[`../pages/${name}.tsx`]
     if (!page) {
       console.error(`Missing Inertia page component: '${name}.tsx'`)
@@ -35,7 +38,7 @@ void createInertiaApp({
     createRoot(el).render(
       <StrictMode>
         <App {...props} />
-      </StrictMode>
+      </StrictMode>,
     )
   },
 
@@ -59,8 +62,8 @@ void createInertiaApp({
   } else {
     console.error(
       "Missing root element.\n\n" +
-      "If you see this error, it probably means you loaded Inertia.js on non-Inertia pages.\n" +
-      'Consider moving <%= vite_typescript_tag "inertia.tsx" %> to the Inertia-specific layout instead.',
+        "If you see this error, it probably means you loaded Inertia.js on non-Inertia pages.\n" +
+        'Consider moving <%= vite_typescript_tag "inertia.tsx" %> to the Inertia-specific layout instead.',
     )
   }
 })
--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,8 @@
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-import { defineConfig } from 'vite'
-import RubyPlugin from 'vite-plugin-ruby'
+import react from "@vitejs/plugin-react"
+import tailwindcss from "@tailwindcss/vite"
+import { defineConfig } from "vite"
+import RubyPlugin from "vite-plugin-ruby"
 
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-    RubyPlugin(),
-  ],
+  plugins: [react(), tailwindcss(), RubyPlugin()],
 })
