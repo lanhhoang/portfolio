fe873239e21a9dbe5f8675c10751d9d7d6b05c0a
Lanh Hoang <lanhhoang@users.noreply.github.com>
2026-01-19 01:07:05 -0500

    feat: Add Home page

---
+++ src/pages/Home.tsx
@@ -0,0 +1,15 @@
+export function Home() {
+  return (
+    <div className="min-h-screen">
+      {/* Theme Toggle */}
+      {/* Background Effects */}
+      {/* Navbar */}
+      {/* Hero Section */}
+      {/* About Section */}
+      {/* Skills Section */}
+      {/* Projects Section */}
+      {/* Contact Section */}
+      {/* Footer */}
+    </div>
+  );
+}
