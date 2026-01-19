80d031657c225785a041c1f2a582e175ba159948
Lanh Hoang <lanhhoang@users.noreply.github.com>
2026-01-19 01:15:46 -0500

    feat: Add Theme Customization

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,6 @@
 export function Home() {
   return (
-    <div className="min-h-screen">
+    <div className="relative min-h-screen bg-white dark:bg-slate-950 text-slate-900 dark:text-white overflow-x-hidden">
       {/* Theme Toggle */}
       {/* Background Effects */}
       {/* Navbar */}
