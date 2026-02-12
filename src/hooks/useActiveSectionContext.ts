42cf29e202eea3e37f51e76edced05dc8b3a2cec
Lanh Hoang <lanhhoang@users.noreply.github.com>
2026-02-12 01:08:58 -0500

    feat: Implement active section tracking using a new context

---
+++ src/hooks/useActiveSectionContext.ts
@@ -0,0 +1,14 @@
+import { useContext } from "react";
+import { ActiveSectionContext } from "@/context/active-section-context";
+
+export function useActiveSectionContext() {
+  const context = useContext(ActiveSectionContext);
+
+  if (context === null) {
+    throw new Error(
+      "useActiveSectionContext must be used within a ActiveSectionContextProvider",
+    );
+  }
+
+  return context;
+}
