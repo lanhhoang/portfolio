87e5b739643b56380bf02ca20ddf734330c2fb85
Lanh Hoang <lanhhoang@users.noreply.github.com>
2026-09-04 15:46:02 -0400

    feat(admin): add protected CMS navigation

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,7 @@
 class Admin::DashboardController < Admin::BaseController
   def show
+    @project_count = Project.count
+    @post_count = Post.count
+    @tag_count = Tag.count
   end
 end
---
+++ app/controllers/admin/projects_controller.rb
@@ -0,0 +1,5 @@
+class Admin::ProjectsController < Admin::BaseController
+  # ponytail: interim stub for the Task 1 shell; Task 4 replaces this file with full CRUD.
+  def index
+  end
+end
